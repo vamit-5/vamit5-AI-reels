17dcd6f11674256fe70ee0899a06f54fda852e82
vamit-5 <kukicborislav@gmail.com>
2026-07-31 14:31:41 +0200

    Create edu_scripts.py

---
+++ vamit5-recovery-reels/lib/edu_scripts.py
@@ -0,0 +1,9 @@
+"""
+Fiksne skripte za EDUKATIVNE (AI anatomija) reels-e. Isti princip kao
+lib/scripts.py -- rotiraju se u krug, tekst se NE menja/izmislja, samo se
+segmentira i za svaki deo generise odgovarajuci AI anatomski video.
+"""
+
+EDU_SCRIPTS = [
+    "Znaš ono kada izađeš iz prodavnice sa dve kese, popneš se na treći sprat i staneš ispred vrata kao da čekaš medicinski tim? Onda uđeš unutra, spustiš kese i kažeš sebi: Moram stvarno da krenem da treniram. I onda prođu tri dana, pojavi se obaveza, loš dan, umor, i opet isto. Problem većine ljudi nije što ne znaju šta treba da rade. Problem je što nemaju sistem koji ih svaki dan pokrene. Tvoje telo se menja kada dobija pravi izazov dovoljno dugo. Jači mišići, bolja kondicija i više energije dolaze iz ponavljanja pravih stvari. VAMIT-5 je napravljen da ti ukloni najveću prepreku, da ne moraš svaki dan da smišljaš šta da radiš i kako da se nateraš. Dobijaš strukturiran sistem, novi izazov svaki dan, glas koji te vodi kroz trening od prve do poslednje sekunde i zajednicu ljudi koja prolazi isti put. Jer najteži deo transformacije nije jedan težak trening. Najteži deo je ostati osoba koja se pojavljuje svaki dan. Da li si ajkula ili ribica? Testiraj VAMIT-5 App 7 dana potpuno besplatno. Link je u opisu profila.",
+]
